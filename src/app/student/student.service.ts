c21f11f27a0adb77f78d45be3ec6e4d26a388084
Cody McCodePants <cody@mccodepants.com>
2024-01-28 19:02:22 -0600

    Linked

--- src/app/student/student.service.ts
+++ src/app/student/student.service.ts
@@ -14,19 +14,19 @@ export class StudentService {
   }
 
   public findAll(): Observable<Student[]> {
-    return this.http.get<Student[]>(`/api/students/`);
+    return this.http.get<Student[]>(`https://techmapback.onrender.com/api/students/`);
   }
 
   public addStudent(student: Student): Observable<Student> {
-    return this.http.post<Student>(`/api/students/add/`, student);
+    return this.http.post<Student>(`https://techmapback.onrender.com/api/students/add/`, student);
   }
 
   public updateStudent(student: Student): Observable<Student> {
-    return this.http.put<Student>(`/api/students/update/`, student)
+    return this.http.put<Student>(`https://techmapback.onrender.com/api/students/update/`, student)
   }
 
   deleteStudent(id: number) {
-    const url = `/api/students/${id}/`;
+    const url = `https://techmapback.onrender.com/api/students/${id}/`;
     return this.http.delete(url);
   }
 
