0d193dcd674804dea89262d0ac09489c0e1ff13e
Cody McCodePants <cody@mccodepants.com>
2024-01-28 18:00:28 -0600

    Finished, ready for merge

--- src/app/field/field.module.ts
+++ src/app/field/field.module.ts
@@ -6,6 +6,19 @@ import {TableModule} from "primeng/table";
 import {CardModule} from "primeng/card";
 import {ButtonModule} from "primeng/button";
 import {RouterLink} from "@angular/router";
+import {HeaderModule} from "../header/header.module";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {DialogModule} from "primeng/dialog";
+import {DropdownModule} from "primeng/dropdown";
+import {FileUploadModule} from "primeng/fileupload";
+import {FormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {RippleModule} from "primeng/ripple";
+import {ToastModule} from "primeng/toast";
+import {ToolbarModule} from "primeng/toolbar";
+import {ConfirmationService} from "primeng/api";
+import {AppComponent} from "../app.component";
+import {AppModule} from "../app.module";
 
 
 
@@ -14,13 +27,24 @@ import {RouterLink} from "@angular/router";
     EditFieldsComponent,
     FieldTableComponent
   ],
-  imports: [
-    CommonModule,
-    TableModule,
-    CardModule,
-    ButtonModule,
-    RouterLink
-  ],
-  exports: [EditFieldsComponent, FieldTableComponent]
+    imports: [
+        CommonModule,
+        TableModule,
+        CardModule,
+        ButtonModule,
+        RouterLink,
+        HeaderModule,
+        ConfirmDialogModule,
+        DialogModule,
+        DropdownModule,
+        FileUploadModule,
+        FormsModule,
+        InputTextModule,
+        RippleModule,
+        ToastModule,
+        ToolbarModule,
+    ],
+  exports: [EditFieldsComponent, FieldTableComponent],
+  providers: [ConfirmationService]
 })
 export class FieldModule { }
